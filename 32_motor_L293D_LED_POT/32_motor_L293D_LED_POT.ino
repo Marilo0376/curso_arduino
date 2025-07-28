1a3a175ca7a111501080647d1fec9f857be16539
Pablo <pablo7escalante@gmail.com>
2025-07-28 13:07:17 +0200

    28.07.25

---
+++ 32_motor_L293D_LED_POT/32_motor_L293D_LED_POT.ino
@@ -0,0 +1,97 @@
+// https://descubrearduino.com/l293d/
+
+#define VELOCIDAD 14
+#define DIRPIN_1 12
+#define DIRPIN_2 13
+
+//Estados posibles
+#define PARO 0
+#define DERECHA 1
+#define IZQUIERDA 2
+
+#define PIN_VERDE 15
+#define PIN_ROJO 4
+#define PIN_AZUL 5
+
+#define POT 0
+
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(VELOCIDAD, OUTPUT);
+  pinMode(DIRPIN_1, OUTPUT);
+  pinMode(DIRPIN_2, OUTPUT);
+
+  pinMode(PIN_ROJO,OUTPUT);
+  pinMode(PIN_VERDE,OUTPUT);
+  pinMode(PIN_AZUL,OUTPUT);
+
+  pinMode (POT, INPUT);
+}
+
+void loop() {
+  cambioEstado(DERECHA);
+  delay(5000);
+
+  cambioEstado(PARO);
+  delay(2000);
+
+  cambioEstado(IZQUIERDA);
+  delay(5000);
+
+  cambioEstado(PARO);
+  delay(2000);
+}
+
+void cambioEstado(int estadoNuevo){
+  switch(estadoNuevo){
+    case PARO:
+      Serial.println("Motor parado");
+      analogWrite(VELOCIDAD, 0); // Aapagamos el motor
+      digitalWrite(PIN_ROJO, HIGH);
+      digitalWrite(PIN_AZUL, LOW);
+      digitalWrite(PIN_VERDE, LOW);
+      break;
+
+    case IZQUIERDA:
+      //digitalWrite(VELOCIDAD, HIGH); // Encendemos el motor
+      Serial.println("Girando en la dirección IZQUIERDA.");
+      analogWrite(VELOCIDAD, leerPot());
+      digitalWrite(DIRPIN_1, 0);
+      digitalWrite(DIRPIN_2, 1);
+      digitalWrite(PIN_ROJO, LOW);
+      digitalWrite(PIN_AZUL, HIGH);
+      digitalWrite(PIN_VERDE, LOW);
+      break;
+      
+    case DERECHA:
+      //digitalWrite(VELOCIDAD, HIGH); // Encendemos el motor
+      Serial.println("Girando en la dirección DERECHA.");
+      analogWrite(VELOCIDAD, leerPot());
+      digitalWrite(DIRPIN_1, 1);
+      digitalWrite(DIRPIN_2, 0);   
+      digitalWrite(PIN_ROJO, LOW);
+      digitalWrite(PIN_AZUL, LOW);
+      digitalWrite(PIN_VERDE, HIGH); 
+      break;
+  }
+}
+  int leerPot(){
+    int valorPot = 0;
+    int valorVel = 0;
+    valorPot = analogRead(POT);
+    valorVel = map(valorPot,0,4096,80,255);
+
+    Serial.println("Velocidad: " + String(valorVel));
+
+    return valorVel;
+
+}
+
+
+
+
+
+
+  
